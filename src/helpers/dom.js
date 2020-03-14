79883ccfa5cb8b81293b3d9d86c469f773449124
sarah <sarachamorro@gmail.com>
2020-03-13 21:18:28 -0300

    Add tab icon and readme template

--- src/helpers/dom.js
+++
@@ -1,9 +0,0 @@
-const cleanKeyPress = () => {
-  window.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-    }
-  }, false);
-};
-
-export default cleanKeyPress;
