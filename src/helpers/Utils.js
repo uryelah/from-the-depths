c8272472861a232f6ff159b298961cca1e30e49f
sarah <sarachamorro@gmail.com>
2020-04-06 13:28:42 -0300

    Linting fixes.

--- src/helpers/Utils.js
+++ src/helpers/Utils.js
@@ -1,56 +1,54 @@
-const Utils = (() => {
-  return {
-    absSubstract: (a, b) => Math.abs(Math.abs(a) - Math.abs(b)),
+const Utils = (() => ({
+  absSubstract: (a, b) => Math.abs(Math.abs(a) - Math.abs(b)),
 
-    animateBg: (context ,frames) => {
-      if (frames % 20 === 0) {
-        if (context.ts.currentFrame + 1 < context.ts.frameLength) {
-          context.ts.setFrame(context.ts.currentFrame + 1);
-          context.ts.currentFrame += 1;
-        } else {
-          context.ts.setFrame(0);
-          context.ts.currentFrame = 0;
-        }
+  animateBg: (context, frames) => {
+    if (frames % 20 === 0) {
+      if (context.ts.currentFrame + 1 < context.ts.frameLength) {
+        context.ts.setFrame(context.ts.currentFrame + 1);
+        context.ts.currentFrame += 1;
+      } else {
+        context.ts.setFrame(0);
+        context.ts.currentFrame = 0;
       }
-    },
-
-    refreshStatus: (type = { score: true, hp: true }, element) => {
-      const { score, hp } = type;
-
-      if (score) {
-        element.score && element.score.destroy();
-        element.score = element.add.text(10, 20, `Points: ${element.player.getScore()}`, { fontFamily: '"Roboto Condensed"' }).setColor('red');
-      }
-
-      if (hp) {
-        element.hp && element.hp.destroy();
-        element.hp = element.add.text(10, 40, `HP: ${element.player.getHp().toFixed(2)}`, { fontFamily: '"Roboto Condensed"' }).setColor('yellow');
-      }
-    },
-
-    endGame: (context, interval) => {
-      clearInterval(interval);
-      context.over = true;
-      context.sfx.bgm.stop();
-      context.game.scene.stop('TentacleTest');
-      context.game.scene.start('SceneGameOver', { score: context.player.getScore(), name: context.name });
-
-      fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${context.id}/scores`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          user: context.name,
-          score: context.player.getScore(),
-        }),
-      })
-        .then((data) => data.json()).then((res) => res.result)
-        .catch((error) => {
-          console.log('Request failure: ', error);
-        });
     }
-  }
-})();
+  },
+
+  refreshStatus: (type = { score: true, hp: true }, element) => {
+    const { score, hp } = type;
+
+    if (score) {
+      element.score && element.score.destroy();
+      element.score = element.add.text(10, 20, `Points: ${element.player.getScore()}`, { fontFamily: '"Roboto Condensed"' }).setColor('red');
+    }
+
+    if (hp) {
+      element.hp && element.hp.destroy();
+      element.hp = element.add.text(10, 40, `HP: ${element.player.getHp().toFixed(2)}`, { fontFamily: '"Roboto Condensed"' }).setColor('yellow');
+    }
+  },
+
+  endGame: (context, interval) => {
+    clearInterval(interval);
+    context.over = true;
+    context.sfx.bgm.stop();
+    context.game.scene.stop('TentacleTest');
+    context.game.scene.start('SceneGameOver', { score: context.player.getScore(), name: context.name });
+
+    fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${context.id}/scores`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user: context.name,
+        score: context.player.getScore(),
+      }),
+    })
+      .then((data) => data.json()).then((res) => res.result)
+      .catch((error) => {
+        console.log('Request failure: ', error);
+      });
+  },
+}))();
 
 export default Utils;
